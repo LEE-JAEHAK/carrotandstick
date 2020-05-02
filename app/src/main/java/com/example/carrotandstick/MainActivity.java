68871bf3ff1fb47c903658421d43d1417be8dc8a
jerry <wogkr101@gmail.com>
2020-05-03 01:52:49 +0900

    Initial commit

---
+++ app/src/main/java/com/example/carrotandstick/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.carrotandstick;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.carrotandstick.calendar.CalendarFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    MainFragment mainFragment;
+    CalendarFragment calendarFragment;
+    PrizeFragment prizeFragment;
+    MypageFragment mypageFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottom_menu);
+
+        mainFragment = new MainFragment();
+        calendarFragment = new CalendarFragment();
+        prizeFragment = new PrizeFragment();
+        mypageFragment = new MypageFragment();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_framelayout, mainFragment).commitAllowingStateLoss();
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    //menu_bottom.xml에서 지정해줬던 아이디 값을 받아와서 각 아이디값마다 다른 이벤트를 발생시킵니다.
+                    case R.id.underbar_btn_main: {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_framelayout, mainFragment).commitAllowingStateLoss();
+                        return true;
+                    }
+                    case R.id.underbar_btn_calendar: {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_framelayout, calendarFragment).commitAllowingStateLoss();
+                        return true;
+                    }
+                    case R.id.underbar_btn_prize: {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_framelayout, prizeFragment).commitAllowingStateLoss();
+                        return true;
+                    }
+                    case R.id.underbar_btn_mypage: {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_framelayout, mypageFragment).commitAllowingStateLoss();
+                        return true;
+                    }
+                    default:
+                        return false;
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/carrotandstick/calendar/GridviewAdapter.java
@@ -0,0 +1,55 @@
+package com.example.carrotandstick.calendar;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.carrotandstick.R;
+
+import java.util.ArrayList;
+
+public class GridviewAdapter extends BaseAdapter {
+    ArrayList<Item> itemArrayList;
+    LayoutInflater layoutInflater;
+    Context context;
+
+    public GridviewAdapter(ArrayList<Item> itemArrayList, Context context) {
+        this.itemArrayList = itemArrayList;
+        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return itemArrayList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return itemArrayList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        convertView = layoutInflater.inflate(R.layout.goalitem_gridview, parent, false);
+
+        Item item = itemArrayList.get(position);
+
+        TextView textView = convertView.findViewById(R.id.calendar_tv_dday);
+        TextView textView1 = convertView.findViewById(R.id.calendar_tv_goal);
+
+        textView.setText(item.getDday());
+        textView1.setText(item.getGoal());
+
+
+        return convertView;
+    }
+}
